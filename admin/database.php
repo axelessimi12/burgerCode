f4e91d317bf2a91dbd2dd604fc634eaea7b6f09c
axelessimi12 <axelessimi@gmail.com>
2022-08-10 11:13:49 +0200

    burger code project file

---
+++ admin/database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database{
+    private static $dbHost = "localhost";
+    private static $dbName = "burger_code";
+    private static $dbUser = "root";
+    private static $dbPassword = "";
+
+    private static $connection = null;
+
+    public static function connect(){
+        try
+        {
+            self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPassword);
+        }
+        catch(PDOException $e){
+            die($e->getMessage());
+        }
+    
+        return self::$connection;
+         }
+    
+        public static function disconnect(){
+            self::$connection = null;
+        }
+}
+
+
+
+
+    Database::connect();
+?>
